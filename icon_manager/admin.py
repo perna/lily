2687652f8dbadc1997f7f1a3f217c08c1bb4e49e
Anderson Meira <anderson.meira@gmail.com>
2018-10-04 19:26:59 -0300

    url download configuration and admin panel of Icons

--- icon_manager/admin.py
+++ icon_manager/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from .models import Icon, IconFile, IconType
 
+class IconFileInline(admin.TabularInline):
+    model = IconFile
+    extra = 2
+    list_display = ['files__name']
+
 class IconAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ('name', 'tags')
     prepopulated_fields = {'slug': ('name',)}
+    inlines = [ IconFileInline ]
 
-
-class IconFileAdmin(admin.ModelAdmin):
-    list_display = ('icon', 'file_extension')
-    list_filter = ('file_extension', 'icon')
 
 class IconTypeAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Icon, IconAdmin)
-admin.site.register(IconFile, IconFileAdmin)
 admin.site.register(IconType, IconTypeAdmin)
