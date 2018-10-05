ef6bccc2c057fb18e4a3df8c7432b3cfdc2c6744
Anderson Meira <anderson.meira@gmail.com>
2018-10-05 18:53:58 -0300

    refactoring Icon model

--- icon_manager/models.py
+++ icon_manager/models.py
@@ -4,9 +4,9 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 class Icon(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(verbose_name='Nome', max_length=64, unique=True)
     tags = ArrayField(models.CharField(max_length=128), blank=True)
-    slug = models.SlugField(max_length=255, unique=True)
+    slug = models.SlugField(verbose_name='Slug', max_length=255, unique=True)
 
     class Meta:
         db_table = "icons"
@@ -16,25 +16,33 @@ class Icon(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return 'details/{}'.format(self.slug)
 
-class IconType(models.Model):
-    name = models.CharField(max_length=32)
-    extension = models.CharField(max_length=8)
-
-    class Meta:
-        db_table = "icon_types"
-        verbose_name = "Tipo do icone"
-        verbose_name_plural = "Tipos dos icones"
-
-    def __str__(self):
-        return self.name
 
 
 class IconFile(models.Model):
+    # file formats
+    AI = 'AI'
+    EPS = 'EPS'
+    JPG = 'JPG'
+    PDF = 'PDF'
+    PNG = 'PNG'
+    SVG = 'SVG'
+
+    filetype_choices = (
+        (AI, 'ai'),
+        (EPS, 'eps'),
+        (JPG, 'jpg'),
+        (PDF, 'pdf'),
+        (PNG, 'png'),
+        (SVG, 'svg')
+    )
+
     icon = models.ForeignKey(Icon, on_delete=models.CASCADE, related_name='files')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    file_extension = models.ForeignKey(IconType, on_delete=models.CASCADE, related_name='files')
-    icon_file = models.FileField(upload_to='icons/')
+    file_extension = models.CharField(verbose_name='Formato', max_length=4, choices=filetype_choices, default=SVG)
+    icon_file = models.FileField(verbose_name='Arquivo', upload_to='icons/')
 
     class Meta:
         db_table = "files"
